8f1ed35cb6c6c20126f8074b23e0790a7c947354
Hristijan Stavrov <hristijan.stavrov@gmail.com>
2022-08-14 18:02:01 +0200

    Add project files.

---
+++ bezbednost2019/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace bezbednost2019
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.browse_button = new System.Windows.Forms.Button();
+            this.filepath = new System.Windows.Forms.TextBox();
+            this.delete_button = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.logs = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.browse_button_sd = new System.Windows.Forms.Button();
+            this.sdpath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // browse_button
+            // 
+            this.browse_button.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.browse_button.Location = new System.Drawing.Point(447, 16);
+            this.browse_button.Name = "browse_button";
+            this.browse_button.Size = new System.Drawing.Size(75, 23);
+            this.browse_button.TabIndex = 0;
+            this.browse_button.Text = "Browse";
+            this.browse_button.UseVisualStyleBackColor = true;
+            this.browse_button.Click += new System.EventHandler(this.browse_button_Click);
+            // 
+            // filepath
+            // 
+            this.filepath.Location = new System.Drawing.Point(6, 19);
+            this.filepath.Name = "filepath";
+            this.filepath.ReadOnly = true;
+            this.filepath.Size = new System.Drawing.Size(426, 20);
+            this.filepath.TabIndex = 1;
+            // 
+            // delete_button
+            // 
+            this.delete_button.BackColor = System.Drawing.Color.Maroon;
+            this.delete_button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.delete_button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.delete_button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Crimson;
+            this.delete_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.delete_button.ForeColor = System.Drawing.Color.White;
+            this.delete_button.Location = new System.Drawing.Point(528, 16);
+            this.delete_button.Name = "delete_button";
+            this.delete_button.Size = new System.Drawing.Size(75, 23);
+            this.delete_button.TabIndex = 3;
+            this.delete_button.Text = "Erase file";
+            this.delete_button.UseVisualStyleBackColor = false;
+            this.delete_button.Click += new System.EventHandler(this.delete_button_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // logs
+            // 
+            this.logs.Location = new System.Drawing.Point(6, 19);
+            this.logs.Name = "logs";
+            this.logs.ReadOnly = true;
+            this.logs.Size = new System.Drawing.Size(597, 179);
+            this.logs.TabIndex = 4;
+            this.logs.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.logs);
+            this.groupBox1.Location = new System.Drawing.Point(12, 155);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(609, 204);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Logs";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(635, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.helpToolStripMenuItem.Text = "Help!";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.linkLabel1);
+            this.groupBox2.Controls.Add(this.browse_button_sd);
+            this.groupBox2.Controls.Add(this.sdpath);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.progressBar1);
+            this.groupBox2.Controls.Add(this.browse_button);
+            this.groupBox2.Controls.Add(this.delete_button);
+            this.groupBox2.Controls.Add(this.filepath);
+            this.groupBox2.Location = new System.Drawing.Point(12, 27);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(609, 122);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Select file for erasing";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(6, 51);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(85, 13);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "SDelete location";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // browse_button_sd
+            // 
+            this.browse_button_sd.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.browse_button_sd.Location = new System.Drawing.Point(447, 65);
+            this.browse_button_sd.Name = "browse_button_sd";
+            this.browse_button_sd.Size = new System.Drawing.Size(75, 23);
+            this.browse_button_sd.TabIndex = 8;
+            this.browse_button_sd.Text = "Browse";
+            this.browse_button_sd.UseVisualStyleBackColor = true;
+            this.browse_button_sd.Click += new System.EventHandler(this.browse_button_sd_Click);
+            // 
+            // sdpath
+            // 
+            this.sdpath.Location = new System.Drawing.Point(6, 67);
+            this.sdpath.Name = "sdpath";
+            this.sdpath.ReadOnly = true;
+            this.sdpath.Size = new System.Drawing.Size(426, 20);
+            this.sdpath.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(528, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Clear Logs";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Enabled = false;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(555, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0%";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 93);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(516, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(635, 371);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Eraser";
+            this.groupBox1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox filepath;
+        private System.Windows.Forms.Button delete_button;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox logs;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button browse_button;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button browse_button_sd;
+        private System.Windows.Forms.TextBox sdpath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    }
+}
+
---
+++ bezbednost2019/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace bezbednost2019
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] hardlinks;
+
+        public void printtime()
+        {
+            logs.Text += "[" + DateTime.Now.ToString("HH:mm:ss") + "] ";
+        }
+
+        public void helperprocess(string command, string arguments, bool mode)
+        {
+            using (Process hl_proc = new Process())
+            {
+                hl_proc.StartInfo.FileName = command;
+                hl_proc.StartInfo.Arguments = arguments;
+                hl_proc.StartInfo.Verb = "runas";
+                hl_proc.StartInfo.UseShellExecute = false;
+                hl_proc.StartInfo.RedirectStandardOutput = mode;
+                hl_proc.StartInfo.CreateNoWindow = mode;
+                hl_proc.Start();
+                if (mode)
+                {
+                    StreamReader reader = hl_proc.StandardOutput;
+                    string output = reader.ReadToEnd();
+                    hl_proc.WaitForExit();
+                    if (command == "fsutil.exe")
+                    {
+                        hardlinks = output.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                    }
+                    printtime();
+                    logs.Text += output + "\n";
+                }
+                hl_proc.WaitForExit();
+            }
+        }
+
+        public void accepteula(string command)
+        {
+            using (Process proc = new Process())
+            {
+                proc.StartInfo.FileName = command;
+                proc.StartInfo.Arguments = string.Format("/accepteula");
+                proc.StartInfo.Verb = "runas";
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.RedirectStandardOutput = false;
+                proc.StartInfo.CreateNoWindow = true;
+                proc.Start();
+                proc.WaitForExit();
+            }
+        }
+
+        public void brisi(string filename)
+        {
+            try
+            {
+                if (File.Exists(filename))
+                {
+                    File.SetAttributes(filename, FileAttributes.Normal);
+                    double sectors = Math.Ceiling(new FileInfo(filename).Length / 512.0);
+                    byte[] zeroBuffer = new byte[512];
+                    Stream inputStream;
+                    try
+                    {
+                        inputStream = new FileStream(
+                            filename,
+                            FileMode.Open,
+                            FileAccess.Write,
+                            FileShare.None,
+                            (int)filename.Length,
+                            FileOptions.DeleteOnClose | FileOptions.RandomAccess | FileOptions.WriteThrough);
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        return;
+                    }
+                    using (inputStream)
+                    {
+                        for (int sectorsWritten = 0; sectorsWritten < sectors; sectorsWritten++)
+                        {
+                            progressBar1.Increment(Convert.ToInt32(75 / sectors));
+                            label1.Text = progressBar1.Value.ToString() + "%";
+                            inputStream.Write(zeroBuffer, 0, zeroBuffer.Length);
+                        }
+                        inputStream.SetLength(0);
+                        inputStream.Close();
+                        printtime();
+                        logs.Text += "Deleted " + filename + "\n";
+                    }
+                    checker();
+                    MessageBox.Show("Completed");
+                }
+            }
+            catch (Exception e)
+            {
+                printtime();
+                logs.Text += e;
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void browse_button_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filepath.Text = openFileDialog1.FileName;
+            }
+            progBarInit();
+        }
+
+        private void browse_button_sd_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                sdpath.Text = openFileDialog2.FileName;
+            }
+        }
+
+        private void delete_button_Click(object sender, EventArgs e)
+        {
+            this.timer1.Start();
+            if (filepath.Text != "" && File.Exists(filepath.Text))
+            {
+                DialogResult okcancle = MessageBox.Show($"Are you sure you want to erase this file {filepath.Text}?", "Continue", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                if (okcancle == DialogResult.OK)
+                {
+                    printtime();
+                    logs.Text += $"File selected for deletion {filepath.Text}\n";
+                    progressBar1.Increment(5);
+                    label1.Text = progressBar1.Value.ToString() + "%";
+                    printtime();
+                    logs.Text += "Hard links:\n";
+                    helperprocess("fsutil.exe", "hardlink list " + filepath.Text, true);
+                    progressBar1.Increment(5);
+                    label1.Text = progressBar1.Value.ToString() + "%";
+                    foreach (string s in hardlinks)
+                    {
+                        string fullPath = Path.GetFullPath(filepath.Text[0] + ":" + s);
+                        if (fullPath != filepath.Text && File.Exists(fullPath))
+                        {
+                            File.Delete(fullPath);
+                            printtime();
+                            logs.Text += $"Deleted hard link: {fullPath}\n";
+                        }
+                    }
+                    progressBar1.Increment(5);
+                    label1.Text = progressBar1.Value.ToString() + "%";
+                    if (File.Exists(sdpath.Text))
+                    {
+                        accepteula(sdpath.Text);
+                        helperprocess(sdpath.Text, " -p 4 " + filepath.Text, true);
+                        progressBar1.Increment(20);
+                        label1.Text = progressBar1.Value.ToString() + "%";
+                        helperprocess(sdpath.Text, " -z " + filepath.Text.Substring(0, 3), false);
+                        progressBar1.Increment(75);
+                        label1.Text = progressBar1.Value.ToString() + "%";
+                        MessageBox.Show("Completed");
+
+                    }
+                    else
+                    {
+                        printtime();
+                        logs.Text += "Starting deletion " + filepath.Text + "\n";
+                        brisi(filepath.Text);
+                    }
+                }
+            }
+            else
+            {
+                this.timer1.Stop();
+                MessageBox.Show("Please select a file!", "Error - No file selected", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                printtime();
+                logs.Text += "No file selected\n";
+            }
+            printtime();
+            logs.Text += "Completed\n";
+            filepath.Text = "";
+            this.timer1.Stop();
+            button1.Enabled = true;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("IMPORTANT: MAKE SURE NOT TO HAVE A SPACEBAR IN THE FOLDER OR FILE YOU WANT TO DELETE" + "\n" + " We simply select the file that we want to delete then " + "\n" + "We have 2 options:" + "\n" +
+                " A: Use my method of secure delete(that works with filling the file with 0's and then it deletes it." + "\n" +
+                " B: Use the Sdelete (I just added this option because I wanted to test it.)  " + "\n" + " NOTE: When using the Sdelet you must select it where you have unpacked it or simply use the source I have provided in the Sdelete folder. ", "Tutorial", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = progressBar1.Value.ToString() + "%";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            logs.Text = string.Empty;
+            button1.Enabled = false;
+            progressBar1.Value = 0;
+            label1.Text = "0%";
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://docs.microsoft.com/en-us/sysinternals/downloads/sdelete");
+        }
+
+        private void progBarInit()
+        {
+            progressBar1.Value = 0;
+            label1.Text = "0%";
+        }
+        private void checker()
+        {
+            if (progressBar1.Value != 100)
+                progressBar1.Value = 100;
+            label1.Text = progressBar1.Value.ToString() + "%";
+        }
+    }
+}
